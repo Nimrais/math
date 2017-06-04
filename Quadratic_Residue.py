40b83e9c10f239c5f251ece89ea4284a758ca474
Nikola <nikola.lukashuk@gmail.com>
2017-06-04 15:15:38 +0300

    Function is_quadratic_residue

---
+++ Quadratic_Residue.py
@@ -0,0 +1,11 @@
+def create_is_qv(n):
+    def is_Quadratic_residue(k):
+        return (k**((n-1)//2))%n == 1
+    return is_Quadratic_residue
+if __name__ == "__main__":
+    list_of_is = []
+    for i in [5,7,13]:
+        list_of_is.append(create_is_qv(i))
+    for func in list_of_is:
+        assert func(1) == True, "Mistake in is_quadratic_residue"
+        
