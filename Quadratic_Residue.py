46f6191f54692fe253e11b18d259f108cb7c5dcf
Nikola <nikola.lukashuk@gmail.com>
2017-06-04 15:18:19 +0300

    Comments

--- Quadratic_Residue.py
+++ Quadratic_Residue.py
@@ -1,3 +1,7 @@
+# How you can see function "create is qv" create new function
+# that for geted prime chose quadratic or not residue
+#
+#
 def create_is_qv(n):
     def is_Quadratic_residue(k):
         return (k**((n-1)//2))%n == 1
@@ -8,4 +12,3 @@ if __name__ == "__main__":
         list_of_is.append(create_is_qv(i))
     for func in list_of_is:
         assert func(1) == True, "Mistake in is_quadratic_residue"
-        
